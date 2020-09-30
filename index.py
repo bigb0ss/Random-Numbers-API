9db0ce832145770d45ef52e99f1b10124c852b58
Shri Charan <36787916+bigb0ss@users.noreply.github.com>
2020-09-30 11:17:22 +0530

    Update index.py

--- index.py
+++ index.py
@@ -3,13 +3,15 @@ from flask_cors import CORS
 
 import numpy as np
 app = Flask(__name__)
-CORS(app)
 
-@app.route('/')
+
+@app.route('/',methods=['GET','POST'])
 def root():
 	num = np.random.randint(100)
 	d={"number":num}
 	return jsonify(d)
 
 
-app.run(port='107')
+if __name__ =='__main__':
+	app.run()
+
