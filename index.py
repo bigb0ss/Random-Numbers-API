e22f64815957b1eeb477f50cf327d67a81c2d98a
Shri Charan <36787916+bigb0ss@users.noreply.github.com>
2020-09-30 11:04:04 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,15 @@
+from flask import Flask,jsonify
+from flask_cors import CORS
+
+import numpy as np
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def root():
+	num = np.random.randint(100)
+	d={"number":num}
+	return jsonify(d)
+
+
+app.run()
