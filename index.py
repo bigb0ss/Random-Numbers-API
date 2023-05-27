6babfe86f84422703e4789e07afc450bb07d3efd
Shri Charan <36787916+bigb0ss@users.noreply.github.com>
2023-05-27 17:30:06 +0100

    Update index.py

--- index.py
+++ index.py
@@ -13,37 +13,6 @@ def root():
 	return jsonify(d)
 
 
-@app.route('/gold')
-def goldprice():
-	page = requests.get("https://www.livechennai.com/gold_silverrate.asp")
-	page_tree = html.fromstring(page.content)
-	gold_price = page_tree.xpath("(//table[@class='table-price'])[2]//font")[1].text
-
-	return jsonify({"price":float(gold_price)})
-
-
-@app.route('/N-News')
-def nationalNews():
-	response = requests.get("https://newsapi.org/v2/top-headlines?country=in&apiKey=67919a9da672474aa169436ef82ce1f2").json()['articles']
-	out = []
-	for res in response:
-		r = {}
-		r['title'] = res['title']
-		r['img'] = res['urlToImage']
-		r['url'] = res['url']
-		r['source'] = res['source']['name']
-		out.append(r)
-
-	return jsonify(out)
-
-@app.route('/silver')
-def silverprice():
-	page = requests.get("https://www.livechennai.com/gold_silverrate.asp")
-	page_tree = html.fromstring(page.content)
-	silver_price = page_tree.xpath("(//table[@class='table-price'])[4]//font")[1].text
-	return jsonify({"price":float(silver_price)})
-
-
 if __name__ =='__main__':
-	app.run()
+	app.run(host='0.0.0.0',port=7000)
 
